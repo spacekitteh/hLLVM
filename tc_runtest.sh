1b67368d6c621372dbe238ff3ff7696dfd842265
Ning Wang <email@ningwang.org>
2015-07-07 09:45:58 -0700

    don't clone if llvm_emp exists

--- tc_runtest.sh
+++ tc_runtest.sh
@@ -4,7 +4,13 @@ LLVM_EMP=~/llvm_emp
 
 sh config.sh
 cabal build
-git clone https://github.com/mlite/llvm_emp ${LLVM_EMP}
+if [ -d ${LLVM_EMP} ]; then
+	cd ${LLVM_EMP}
+	git pull
+else 
+	git clone https://github.com/mlite/llvm_emp ${LLVM_EMP}
+fi
+
 ${MYPATH}/scripts/install.sh ${LLVM_EMP}
 
 ~/llvmsandbox/bin/lnt runtest nt --no-timestamp --sandbox ~/testsandbox --cc ${LLVM_EMP}/debian7_32/bin/clang-emp-hirverify --cxx ${LLVM_EMP}/debian7_32/bin/clang++-emp-hirverify --test-suite ~/test-suite-3.5.0.src
