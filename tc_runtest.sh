bd8af1c702d0f08abc0044244454226753ac7973
Ning Wang <email@ningwang.org>
2015-07-07 09:27:40 -0700

    run llvm-test-suites to verify hir

--- tc_runtest.sh
+++ tc_runtest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 MYPATH=$(dirname $(readlink -f $0))
+LLVM_CMP=~/llvm_cmp
 
+sh config.sh
 cabal build
-git clone https://github.com/mlite/llvm_emp ~/llvm_emp
-${MYPATH}/scripts/install.sh ~/llvm_emp
+git clone https://github.com/mlite/llvm_emp ${LLVM_CMP}
+${MYPATH}/scripts/install.sh ${LLVM_CMP}
 
+~/llvmsandbox/bin/lnt runtest nt --no-timestamp --sandbox ~/testsandbox --cc ${LLVM_CMP}/debian7_32/bin/clang-emp-hirverify --cxx ${LLVM_CMP}/debian7_32/bin/clang++-emp-hirverify --test-suite ~/test-suite-3.5.0.src
