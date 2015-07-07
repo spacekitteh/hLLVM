a57bd24595c6a5af661a7fcd5c283811abadf50f
Ning Wang <email@ningwang.org>
2015-07-07 09:42:02 -0700

    fixes a name

--- tc_runtest.sh
+++ tc_runtest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 MYPATH=$(dirname $(readlink -f $0))
-LLVM_CMP=~/llvm_cmp
+LLVM_EMP=~/llvm_emp
 
 sh config.sh
 cabal build
-git clone https://github.com/mlite/llvm_emp ${LLVM_CMP}
-${MYPATH}/scripts/install.sh ${LLVM_CMP}
+git clone https://github.com/mlite/llvm_emp ${LLVM_EMP}
+${MYPATH}/scripts/install.sh ${LLVM_EMP}
 
-~/llvmsandbox/bin/lnt runtest nt --no-timestamp --sandbox ~/testsandbox --cc ${LLVM_CMP}/debian7_32/bin/clang-emp-hirverify --cxx ${LLVM_CMP}/debian7_32/bin/clang++-emp-hirverify --test-suite ~/test-suite-3.5.0.src
+~/llvmsandbox/bin/lnt runtest nt --no-timestamp --sandbox ~/testsandbox --cc ${LLVM_EMP}/debian7_32/bin/clang-emp-hirverify --cxx ${LLVM_EMP}/debian7_32/bin/clang++-emp-hirverify --test-suite ~/test-suite-3.5.0.src
